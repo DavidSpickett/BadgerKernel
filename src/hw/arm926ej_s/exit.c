15d8035f04255b42e76ce3767e7d231dda54b99c
David Spickett <spickettdavid@googlemail.com>
2019-02-16 19:45:37 +0000

    Rewrite the semihosting call in inline assembler to prevent breakage at -O3.

--- src/hw/arm926ej_s/exit.c
+++ src/hw/arm926ej_s/exit.c
@@ -1,6 +1,7 @@
 void qemu_exit() {
-  register int reg0 asm("r0") = 0x18; // angel_SWIreason_ReportException
-  register int reg1 asm("r1") = 0x20026; // ADP_Stopped_ApplicationExit
-  (void)reg0; (void)reg1;
-  asm("svc 0x00123456");  // make semihosting call
+  asm volatile (
+      "ldr r0, =0x18\n\t"    // angel_SWIreason_ReportException
+      "ldr r1, =0x20026\n\t" // ADP_Stopped_ApplicationExit
+      "svc 0x00123456\n\t"   // make semihosting call
+  );
 }
