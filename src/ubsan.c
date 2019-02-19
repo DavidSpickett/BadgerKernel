1c8d21405a588059c4b01e789c975d9e0d8edcd4
David Spickett <spickettdavid@googlemail.com>
2019-02-19 09:45:03 +0000

    Remove fno-sanitize-recover as GCC 4.9.3 doesn't support it. Therefore remove '_abort' from the handlers. They'll still exit qemu as before though.

--- src/ubsan.c
+++ src/ubsan.c
@@ -1,20 +1,20 @@
 #include "semihosting.h"
 #include "print.h"
 
-void __ubsan_handle_add_overflow_abort(void* a, void* b, void* c) { 
-  print("UBSAN: add_overflow_abort\n");
+void __ubsan_handle_add_overflow(void* a, void* b, void* c) {
+  print("UBSAN: add_overflow\n");
   qemu_exit();
   __builtin_unreachable();
 }
 
-void __ubsan_handle_out_of_bounds_abort(void* a, void* b) { 
-  print("UBSAN: out_of_bounds_abort\n"); 
+void __ubsan_handle_out_of_bounds(void* a, void* b) {
+  print("UBSAN: out_of_bounds\n");
   qemu_exit(); 
   __builtin_unreachable();
 }
 
-void __ubsan_handle_type_mismatch_abort(void* a, void* b) { 
-  print("UBSAN: type_mismatch_abort\n");
+void __ubsan_handle_type_mismatch(void* a, void* b) {
+  print("UBSAN: type_mismatch\n");
   qemu_exit();
   __builtin_unreachable();
 }
@@ -25,8 +25,8 @@ void __ubsan_handle_builtin_unreachable(void* a) {
   __builtin_unreachable();
 }
 
-void __ubsan_handle_load_invalid_value_abort(void* a, void* b) {
-  qemu_print("UBSAN: load_invalid_value_abort\n"); 
+void __ubsan_handle_load_invalid_value(void* a, void* b) {
+  qemu_print("UBSAN: load_invalid_value\n");
   qemu_exit(); 
   __builtin_unreachable(); 
 }
