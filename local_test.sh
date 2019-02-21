161e2fa718fe0b9b937f72e6fc98755f3e7f1339
David Spickett <spickettdavid@googlemail.com>
2019-02-21 18:36:36 +0000

    Add script to build and test all configs.

---
+++ local_test.sh
@@ -0,0 +1,10 @@
+set -e
+for platform in "arm" "thumb" "aarch64"; do
+  for opt_level in "0" "3" "s"; do
+    for ubsan in "ON" "OFF"; do
+      cmake . -DBUILD_PLATFORM=${platform} -DOPT_LEVEL=${opt_level} -DUBSAN=${ubsan}
+      make
+      make clean
+    done
+  done
+done
