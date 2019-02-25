156c73f80dfcb84ff8a29e277e65a1c20d4cc10c
David Spickett <spickettdavid@googlemail.com>
2019-02-25 08:28:21 +0000

    On Arm do user stack extent check using the monitor stack.
    We use supervisor mode's stack pointer to save
    a couple of temporary regiters to allow us to decide
    whether it's a thread switch or a semihosting call.
    
    Then for semihosting calls we switch to system mode,
    which shares the sp of user.
    For thread switches we stay in supervisor and check
    the user stack extent. By temporarily switching to system
    and grabing user's stack pointer.
    
    Then once we've done that restore the temporary registers
    from the monitor stack and switch to user mode to do the switch.
    
    Eventually we should do the switch in system mode,
    so we can save all registers not just callee saved.

--- src/hw/arm_virt/startup.s
+++ src/hw/arm_virt/startup.s
@@ -1,12 +1,13 @@
 .global _Reset
 _Reset:
-  mrs r0, cpsr        // set the cpu to user mode
-  bic r0, r0, #0xf    // clear bottom 4 to go from 0x13->0x10
-  msr cpsr, r0
+  ldr r0, =monitor_stack_top // set supervisor mode stack ptr
+  ldr r0, [r0]               // chase to get value
+  mov sp, r0
+  cps #16                    // switch to user mode
 
   /* Can't move the vector table because virt has
      other stuff at the 0xffff0000 address.
      so the vectors are loaded as a seperate object. */
 
-  ldr sp, =stack_top
+  ldr sp, =stack_top         // set user mode stack pointer
   bl entry
--- src/hw/arm_virt/yield.s
+++ src/hw/arm_virt/yield.s
@@ -1,4 +1,6 @@
 .data
+// TODO: dedupe these strings
+           mon_stack_err: .string "Monitor stack underflow!\n"
 unknown_monitor_call_err: .string "Got an unexpected monitor call!\n"
 
 .text
@@ -8,37 +10,50 @@ platform_yield:
 
 .global __platform_yield
 __platform_yield:
+  push {r0-r1}     // push to monitor stack
+
+  /* Check monitor stack. (note that we don't use all of it on Arm */
+  ldr r0, =monitor_stack_top
+  ldr r0, [r0]
+  sub r0, r0, #8   // 2 Arm registers
+  mov r1, sp
+  cmp r0, r1
+  beq monitor_stack_ok
+  ldr r0, =mon_stack_err
+  bl qemu_print
+  b qemu_exit
+monitor_stack_ok:
+
   /* See if this is semihosting or a thread switch */
-  // Note: we do not use r0/r1 here, they might be semihosting args
-  ldr r2, [lr, #-4]        // load svc instruction used
-  ldr r3, =0xFFFFFF        // mask to get code
-  and r2, r2, r3           //
-  ldr r3, =0x00dead        // thread switch
-  cmp r2, r3
+  ldr r0, [lr, #-4]        // load svc instruction used
+  ldr r1, =0xFFFFFF        // mask to get code
+  and r0, r0, r1           //
+  ldr r1, =0x00dead        // thread switch
+  cmp r0, r1
   beq switch_thread
-  ldr r3, =0x123456        // semihosting call
-  cmp r2, r3
+  ldr r1, =0x123456        // semihosting call
+  cmp r0, r1
   beq semihosting
-  ldr r2, =unknown_monitor_call_err
+  ldr r1, =unknown_monitor_call_err
   b qemu_print
   b qemu_exit
 
 semihosting:
-  svc 0x123456
+  pop {r0-r1}
+  cps #0x1f    // system mode (shares sp of user)
+  svc 0x123456 // picked up by Qemu
   bx lr
 
 switch_thread:
-  mrs r0, cpsr        // set the cpu to user mode
-  bic r0, r0, #0xf    // clear bottom 4 to go from 0x13->0x10
-  msr cpsr, r0
-
   /* Check stack extent */
   ldr r0, =thread_stack_offset
   ldr r1, =current_thread
   ldr r0, [r0]        // chase offset
   ldr r1, [r1]        // chase current
   add r0, r1, r0      // get min. valid stack pointer
-  mov r1, sp          // get current sp
+  cps #0x1f           // go to system mode
+  mov r1, sp          // get thread's stack pointer
+  cps #0x13           // back to svc mode
   sub r1, r1, #(12*4) // take away space we want to use
   cmp r0, r1          // is potential sp < min valid sp?
   bhs stack_extent_failed
@@ -46,6 +61,9 @@ switch_thread:
 .global platform_yield_no_stack_check
 platform_yield_no_stack_check:
   // Jump here when yielding into scheduler for the first time
+
+  pop {r0, r1} // pop temps from supervisor stack
+  cps #16      // switch to user mode
 
   /* Save callee saved regs (no sp/pc) */
   push {r4-r12, r14}
