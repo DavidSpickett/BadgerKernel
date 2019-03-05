56142ee8075f67003143e37a3bdf1196f6e4e5de
David Spickett <spickettdavid@googlemail.com>
2019-03-05 22:31:34 +0000

    Re-enable all of the platforms. Remove the deliberate mistake in yielding test and update local test script to use lit.

--- local_test.sh
+++ local_test.sh
@@ -3,7 +3,8 @@ for platform in "arm" "thumb" "aarch64"; do
   for opt_level in "0" "3" "s"; do
     for ubsan in "ON" "OFF"; do
       cmake . -DBUILD_PLATFORM=${platform} -DOPT_LEVEL=${opt_level} -DUBSAN=${ubsan}
-      make
+      make demos
+      lit demos/
       make clean
     done
   done
