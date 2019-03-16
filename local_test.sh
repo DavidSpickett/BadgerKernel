37f901c6ed2ee9339b7c4b7658c200b8a2d770d6
David Spickett <spickettdavid@googlemail.com>
2019-03-16 13:08:01 +0000

    Add support for running Arm Linux binaries via qemu-arm.
    Install:
    gcc-arm-linux-gnueabi qemu-user
    
    Then make run_<demo> as usual. The only difference is the debug
    target isn't defined for this setup.

--- local_test.sh
+++ local_test.sh
@@ -1,8 +1,8 @@
 set -e
-for platform in "arm" "thumb" "aarch64"; do
+for platform in "arm" "arm_linux" "thumb" "aarch64"; do
   for opt_level in "0" "3" "s"; do
     for ubsan in "ON" "OFF"; do
-      cmake . -DBUILD_PLATFORM=${platform} -DOPT_LEVEL=${opt_level} -DUBSAN=${ubsan}
+      cmake . -DBUILD_PLATFORM=${platform} -DOPT_LEVEL=${opt_level} -DUBSAN=${ubsan} -DNATIVE=OFF
       make demos
       lit demos/
       make clean
