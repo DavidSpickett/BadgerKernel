e7c88c81f951e03d648e9c85f5509da9e8fb9591
David Spickett <spickettdavid@googlemail.com>
2019-03-21 23:31:29 +0000

    Replace the timer demo with something that *should* be consistent. It doesn't show disable_timer but it'll do.

--- demos/timer.c
+++ demos/timer.c
@@ -3,51 +3,28 @@
 #include "thread.h"
 #include "semihosting.h"
 
-void thread_work(const char* msg,
-        bool* your_flag,
-        bool* their_flag,
-        int* total_prints
-) {
-  while (1) {
-    if (*your_flag) {
-      /* Disable the timer after we've found the flag
-         to prevent a race condition. If we did:
-         disable timer -> check flag -> enable timer
-         The timer may not fire between enable/disable.
-         Whether it does depends on code layout and machine speed.
-         With this ordering we might be interrupted while
-         reading our flag but that's fine. We'll just pick
-         it up next time aronud the loop.
-         (and there's no need for waiting for interrupts)
-      */
-      disable_timer();
+const char* msgs[] = {
+  "zero",
+  "one",
+  "two",
+  NULL,
+};
+const char** curr_msg = msgs;
 
-      *your_flag = false;
-      *their_flag = true;
-      *total_prints -= 1;
-
-      if (*total_prints < 0) {
-        break;
-      }
-
-      log_event(msg);
-    }
-    enable_timer();
+void thread_work() {
+  if (!*curr_msg) {
+    log_event("three");
+    qemu_exit();
   }
-  // Exit normally to scheduler...
+
+  log_event(*curr_msg);
+  curr_msg++;
+  add_thread(thread_work);
+  enable_timer();
+  while (1) {}
 }
 
 void demo() {
-  bool f1 = true;
-  bool f2 = false;
-  int num_prints = 4;
-
-  struct ThreadArgs args1 = make_args("aardvark", &f1, &f2, &num_prints);
-  add_named_thread_with_args(thread_work, NULL, args1);
-
-  struct ThreadArgs args2 = make_args("zebra", &f2, &f1, &num_prints);
-  add_named_thread_with_args(thread_work, NULL, args2);
-
-  // Scheduler yields into first thread, timer does the rest
+  add_thread(thread_work);
   start_scheduler();
 }
