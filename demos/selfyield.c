6f49b2169b04e0b0fa255049fcdcab16b0836d52
David Spickett <spickettdavid@googlemail.com>
2019-08-27 20:30:57 +0100

    Add a new demo (more of a selftest really) that checks that returning from an interrupt goes back to the right place.
    Currently fails on AArch64 because it skips
    a stack frame on return. Causing all sorts of
    random issues.

---
+++ demos/selfyield.c
@@ -0,0 +1,31 @@
+#include "thread.h"
+#include "timer.h"
+
+/*
+  Check that when switching threads via
+  an interrupt (or something like it)
+  we return to the exact place we left.
+*/
+
+void log_things() {
+  log_event("one");
+
+  // Manual thread switch (current=this thread, next=this thread)
+  // Simulates an interrupt
+#ifdef __thumb__
+  asm volatile ("svc 0xff":::"memory");
+#else
+  asm volatile ("svc 0xdead":::"memory");
+#endif
+
+  log_event("two");
+}
+
+void thread(void) {
+  log_things();
+}
+
+void demo() {
+  add_thread(thread);
+  start_scheduler();
+}
