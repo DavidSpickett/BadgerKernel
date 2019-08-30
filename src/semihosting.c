c5102ae4f958b28d9e70de6eb8bd1b663bb3d886
David Spickett <spickettdavid@googlemail.com>
2019-08-30 22:40:51 +0100

    Remove redundant else.

--- src/semihosting.c
+++ src/semihosting.c
@@ -3,7 +3,7 @@
 unsigned get_semihosting_event(int status) {
   if (status == 0) {
     return 0x20026; //ADP_Stopped_ApplicationExit
-  } else {
-    return 0x20024; //ADP_Stopped_InternalError
   }
+
+  return 0x20024; //ADP_Stopped_InternalError
 }
