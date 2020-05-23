1209092059dd09ebbfeec5aebe85cbd206b654e0
David Spickett <spickettdavid@googlemail.com>
2020-05-23 10:25:23 +0100

    Fix UBSAN assert on Thumb O3 LTO
    This was because we were actually using
    more than the 64k of RAM the thumb board
    provides.
    
    To solve this move the text section to
    before RAM and add an assert after BSS
    so we'll catch this in future at build time.
    
    Update the local test script to match CI
    while I'm here.

--- local_test.sh
+++ local_test.sh
@@ -5,9 +5,10 @@ for platform in "arm" "thumb" "aarch64" "linux"; do
   for opt_level in "0" "3" "s"; do
     for sanitizers in "ON" "OFF"; do
       for lto in "ON" "OFF"; do
+        rm -f CMakeCache.txt
         cmake . -DBUILD_PLATFORM=${platform} -DOPT_LEVEL=${opt_level} -DSANITIZERS=${sanitizers} -DLTO=${lto}
-        make demos
-        lit demos/
+        make make_demos
+        lit demos/ -a
         make clean
       done
     done
