6f7db2f425419c3bf5f7748c15b78123c5da7830
David Spickett <spickettdavid@googlemail.com>
2020-06-01 16:49:25 +0100

    Handle (aka ignore) arrow keys
    Will do something useful later.
    Increase the input buffer so we
    can pickup escape sequences, which
    are >1 char.

--- demos/shell/shell.c
+++ demos/shell/shell.c
@@ -5,7 +5,8 @@
 
 static char* first_whitespace(char* str) {
   char* rest = str;
-  while(*rest != ' ') {
+  // Return nullptr if no space to find
+  while((*rest != ' ') && (*rest != '\0')) {
     ++rest;
   }
   return rest;
@@ -51,6 +52,7 @@ static void quit(const char* cmd) {
 }
 
 static void do_command(char* cmd) {
+  printf("Do command: %s\n", cmd);
   char* rest = first_whitespace(cmd);
   // Split cmd and arguments
   *rest = '\0';
@@ -73,39 +75,70 @@ static void do_command(char* cmd) {
 #define PRINT_PROMPT printf("\n$ ");
 
 #define MAX_CMD_LINE 256 // Bold assumptions
+#define INPUT_BUFFER_SIZE 16
 static void command_loop(int input) {
   char cmd_line[MAX_CMD_LINE];
   size_t cmd_line_pos = 0;
   PRINT_PROMPT
 
-  char in[2];
-  in[1] = '\0';
+  char in[INPUT_BUFFER_SIZE];
   while(1) {
-    ssize_t got = read(input, &in, 1);
+    // -1 for null terminator space
+    ssize_t got = read(input, &in, INPUT_BUFFER_SIZE-1);
 
-    // Will read nullptr if no data
-    if (got && in[0]) {
-      switch (in[0]) {
-        case '\r': // Enter
-          cmd_line[cmd_line_pos] = '\0';
-          cmd_line_pos = 0;
-          printf("\n");
-          do_command(cmd_line);
-          PRINT_PROMPT
-          break;
-        case 0x7F: // Backspace
-          if (cmd_line_pos) {
-            cmd_line_pos--;
-            printf("\b \b");
-          }
-          break;
-        default:
-          if (cmd_line_pos < MAX_CMD_LINE) {
-            cmd_line[cmd_line_pos] = in[0];
-            ++cmd_line_pos;
-            printf("%s", in);
-          }
-          break;
+    if (got) {
+      const char* curr = in;
+      while (*curr != '\0') {
+        switch (*curr) {
+          case '\r': // Enter
+            cmd_line[cmd_line_pos] = '\0';
+            cmd_line_pos = 0;
+            printf("\n");
+            do_command(cmd_line);
+            PRINT_PROMPT
+            break;
+          case 0x1B: // Escape char
+            if (*(curr+1) == '[') {
+              switch (*(curr+2)) {
+                case 'A': // Up
+                case 'B': // Down
+                  curr += 2; // Ignore
+                  break;
+                case 'C': // Right / forward
+                  // TODO: cursor pos
+                  curr += 2;
+                  break;
+                case 'D': // Left / back
+                  curr += 2;
+                  break;
+              }
+            } else {
+              printf("Unhandled escape sequence!\n");
+              exit(1);
+            }
+            break;
+          case 0x7F: // Backspace
+            if (cmd_line_pos) {
+              cmd_line_pos--;
+              printf("\b \b");
+            }
+            break;
+          case 0x04: // End of transmission (Ctrl-D)
+            quit(NULL);
+            break;
+          default:
+            if (cmd_line_pos < MAX_CMD_LINE) {
+              cmd_line[cmd_line_pos] = *curr;
+              ++cmd_line_pos;
+              // TODO: directly putchar
+              char out[2];
+              out[0] = *curr;
+              out[1] = '\0';
+              printf("%s", out);
+            }
+            break;
+        }
+        ++curr;
       }
     }
   }
