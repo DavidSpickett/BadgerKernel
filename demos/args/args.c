2dee25c9acb8dc5025bb3a6eb8b27e80aec37516
David Spickett <spickettdavid@googlemail.com>
2020-06-06 09:25:59 +0100

    Update args demo

--- demos/args/args.c
+++ demos/args/args.c
@@ -1,3 +1,4 @@
+#include "user/thread.h"
 #include "thread.h"
 #include "util.h"
 #include <stddef.h>
@@ -23,12 +24,14 @@ void sub_printer(char** words, int num_phrases, unsigned int offset) {
 static const char* words[] = {"food", "alligator", "magazine", "raptor"};
 
 void setup(void) {
-  config.log_scheduler = false;
+  KernelConfig cfg = { .log_scheduler=false,
+                       .destroy_on_stack_err=false};
+  k_set_kernel_config(&cfg);
 
   ThreadArgs p_args = make_args(3, "aardvark", 1, 2);
-  add_named_thread_with_args(printer, "printer", p_args);
+  k_add_named_thread_with_args(printer, "printer", &p_args);
 
   // Only takes 3, just zero the 4th arg
   ThreadArgs sp_args = make_args(words, 4, 2, 0);
-  add_named_thread_with_args(sub_printer, "sub_printer", sp_args);
+  k_add_named_thread_with_args(sub_printer, "sub_printer", &sp_args);
 }
