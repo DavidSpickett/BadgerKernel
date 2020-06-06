15de941683a7858b2512dad0dc860cf225742801
David Spickett <spickettdavid@googlemail.com>
2020-06-06 14:28:04 +0100

    Resolve a few TODOs

--- include/user/thread.h
+++ include/user/thread.h
@@ -27,13 +27,7 @@ void log_event(const char* event, ...);
 // TODO: more extensible interface for this?
 void set_kernel_config(const KernelConfig* config);
 
-// TODO: this is the first syscall that has a return value
-// but might switch threads. We need to make sure when the thread
-// resumes, it has the return value
 bool yield_to(int tid);
-// TODO: this call just does what the scheduler does anyway
-// it should be removed. I guess it tells you whether there are
-// any more threads left, that's the only unique thing it does
 bool yield_next(void);
 
 bool get_msg(int* sender, int* message);
