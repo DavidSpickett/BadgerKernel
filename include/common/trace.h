93af221ae6a793b8469e0c097762dd29b41154c9
David Spickett <spickettdavid@googlemail.com>
2020-08-15 12:43:37 +0100

    Dedupe some lines in trace.h

--- include/common/trace.h
+++ include/common/trace.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include "thread.h"
 
-#ifdef __aarch64__
 typedef struct {
+#ifdef __aarch64__
   size_t x29;
   size_t x30; // aka lr
   size_t x27;
@@ -40,11 +40,7 @@ typedef struct {
   size_t spsr_el1;
   size_t x0;
   size_t x1;
-  /* patched in by the kernel */
-  size_t sp;
-} __attribute__((packed)) RegisterContext;
 #elif defined __thumb__
-typedef struct {
   size_t r4;
   size_t r5;
   size_t r6;
@@ -61,11 +57,7 @@ typedef struct {
   size_t lr;
   size_t pc;
   size_t xpsr;
-  /* patched in by the kernel */
-  size_t sp;
-} __attribute__((packed)) RegisterContext;
 #else
-typedef struct {
   size_t r0;
   size_t r1;
   size_t r2;
@@ -84,10 +76,10 @@ typedef struct {
   /* aka the exception mode lr */
   size_t pc;
   size_t cpsr;
+#endif
   /* patched in by the kernel */
   size_t sp;
 } __attribute__((packed)) RegisterContext;
-#endif
 
 // To tell the kernel that everything but sp needs
 // to be written back to the thread's stack.
