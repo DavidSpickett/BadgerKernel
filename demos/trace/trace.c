0db2c9f21690b5e28e40be20ae75387ae19f484e
David Spickett <spickettdavid@googlemail.com>
2020-08-29 15:13:08 +0100

    Expand trace demo to show more register modification
    With the change to always saving the thread
    on syscalls, you can modify your own registers.
    
    Also add a thead to show that you can change
    registers even before a thread has run for the
    first time.

--- demos/trace/trace.c
+++ demos/trace/trace.c
@@ -5,6 +5,32 @@
 
 extern void work_finished(void);
 
+static void set_thread_pc(int tid, void* pc) {
+  RegisterContext ctx;
+  get_thread_registers(tid, &ctx);
+  ctx.pc = (size_t)pc;
+  ctx.pc &= ~1; // For Thumb
+  set_thread_registers(tid, ctx);
+}
+
+void jump_here() {
+  log_event("Changed registers!");
+  // No point returning because our LR will be bogus
+  thread_wait();
+}
+
+void pre_jumped() {
+  // Will be redirected by setup
+  __builtin_unreachable();
+}
+
+void jump_self() {
+  // This proves that you can modify your own registers
+  // since the thread is saved before every syscall.
+  set_thread_pc(CURRENT_THREAD, jump_here);
+  __builtin_unreachable();
+}
+
 void tracee() {
   log_event("Working...");
   yield();
@@ -62,5 +88,15 @@ void tracer() {
 }
 
 void setup(void) {
+  set_thread_name(CURRENT_THREAD, "setup");
+
+  // Has registers changed even before running
+  int tid = add_named_thread(pre_jumped, "prejumped");
+  set_thread_pc(tid, jump_here);
+  yield();
+
+  // Changes its own registers
+  add_named_thread(jump_self, "jumpself");
+  yield();
   add_named_thread(tracer, "tracer");
 }
