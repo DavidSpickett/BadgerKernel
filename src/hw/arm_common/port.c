cd629ec8a91a73bff5e5c3b61b6e6a2e74deeaa2
David Spickett <spickettdavid@googlemail.com>
2020-09-27 16:16:37 +0100

    Don't need to disable clang-format here

--- src/hw/arm_common/port.c
+++ src/hw/arm_common/port.c
@@ -18,20 +18,14 @@ void do_svc(SVCCode code) {
 
 size_t generic_semihosting_call(size_t operation, size_t* parameters) {
   size_t ret;
-  // clang-format off
   // We assume that we're already in kernel mode by this point
-  asm volatile (
-    "mov "RCHR"0, %[operation]\n\t"
-    "mov "RCHR"1, %[parameters]\n\t"
-    SEMIHOSTING_CALL"\n\t"
-    "mov %[ret], "RCHR"0\n\t"
-    :[ret]"=r"(ret)
-    :[parameters]"r"(parameters),
-     [operation]"r"(operation)
-    :RCHR"0", RCHR"1", "memory"
-  );
-  // clang-format on
-
+  asm volatile("mov " RCHR "0, %[operation]\n\t"
+               "mov " RCHR "1, %[parameters]\n\t"
+               "" SEMIHOSTING_CALL "\n\t"
+               "mov %[ret], " RCHR "0\n\t"
+               : [ ret ] "=r"(ret)
+               : [ parameters ] "r"(parameters), [ operation ] "r"(operation)
+               : RCHR "0", RCHR "1", "memory");
   return ret;
 }
 
