9918d91b781095484ae32122697736aa734cb72e
David Spickett <spickettdavid@googlemail.com>
2020-09-27 13:15:25 +0100

    Simplify backtrace by dropping partial AArch64 support

--- include/common/trace.h
+++ include/common/trace.h
@@ -5,6 +5,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// Only works properly for Arm
+#if defined __arm__ && !defined __thumb__
 typedef struct {
   const char* name;
   void* start;
@@ -13,5 +15,6 @@ typedef struct {
 
 void print_backtrace(RegisterContext ctx, const Symbol* symbols,
                      size_t num_symbols);
+#endif
 
 #endif /* ifdef COMMON_TRACE_H */
--- src/common/trace.c
+++ src/common/trace.c
@@ -1,24 +1,19 @@
 #include "common/trace.h"
 #include "common/print.h"
 
-/* Backtraces work conistently for Arm with
-   -mapcs-frame and mostly for AArch64.
-   (give or take one frame because some functions
-    don't modify the frame pointer)
+/* Backtraces only work conistently for Arm with
+   -mapcs-frame.
+   AArch64 is mostly consistent but still breaks.
    Thumb doesn't work at all, there doesn't
    seem to be a pattern to how the fp is handled.
 */
 
+#if defined __arm__ && !defined __thumb__
 typedef struct {
-#ifdef __aarch64__
-  size_t fp;
-  size_t lr;
-#else
   size_t fp;
   size_t ip;
   size_t lr;
   size_t pc;
-#endif
 } FrameInfo;
 
 static const char* find_symbol(const Symbol* symbols, size_t num_symbols,
@@ -42,30 +37,21 @@ static const char* find_symbol(const Symbol* symbols, size_t num_symbols,
 void print_backtrace(RegisterContext ctx, const Symbol* symbols,
                      size_t num_symbols) {
   FrameInfo info;
-#ifdef __aarch64__
-  info.fp = ctx.x29;
-  info.lr = ctx.x30;
-#else
   info.pc = ctx.pc;
   info.lr = ctx.lr;
   info.ip = ctx.r12;
   info.fp = ctx.r11;
-#endif
 
   printf("0: 0x%08x (%s)\n", ctx.pc,
          find_symbol(symbols, num_symbols, (void*)ctx.pc));
 
   int depth = 1;
   while (info.fp) {
-#ifdef __aarch64__
-    // AArch64 points to stored fp
-    info = *(FrameInfo*)(info.fp);
-#else
     // Arm points to some way into the frame info
     info = *(FrameInfo*)(info.fp - 12);
-#endif
     printf("%i: 0x%08x (%s)\n", depth, info.lr,
            find_symbol(symbols, num_symbols, (void*)info.lr));
     depth++;
   }
 }
+#endif
