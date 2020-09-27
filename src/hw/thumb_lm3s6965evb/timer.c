a145d5daf4bc2f6ceb219f3152b404023ac404c4
David Spickett <spickettdavid@googlemail.com>
2020-09-27 15:27:07 +0100

    [Thumb] Do timer functions in kernel mode
    Which removes some #ifdefs in arm_common/port.c
    Add CALL_KERNEL macros to all ports along the
    way.

--- src/hw/thumb_lm3s6965evb/timer.c
+++ src/hw/thumb_lm3s6965evb/timer.c
@@ -8,7 +8,7 @@ static volatile uint32_t* NVIC_ISER0 = (volatile uint32_t*)0xE000E100;
 static volatile uint32_t* NVIC_ICER0 = (volatile uint32_t*)0XE000E180;
 #define TIMER_INT_BIT 1 << 15
 
-void thumb_enable_timer(void) {
+void k_enable_timer(void) {
   // Set reload value (23 bits)
   *SYST_RVR = 0x04FFFF;
 
@@ -25,7 +25,7 @@ void thumb_enable_timer(void) {
   asm volatile("" ::: "memory");
 }
 
-void thumb_disable_timer(void) {
+void k_disable_timer(void) {
   // Looks weird but it is write 1 to disable (different reg though)
   *NVIC_ICER0 |= TIMER_INT_BIT;
 
