aa49d4cd494ff079b15b658b54ecfeb648122955
Oscar Shiang <oscar0225@livemail.tw>
2020-11-20 18:47:23 +0800

    Modify exyielding demo to be clearer
    There are 5 changes in this commit:
    
     1. Adjust the names of the worker functions
    
          Change the names of the functions
    
            "thread1" -> "first"
    	"thread2" -> "second"
    
          to be a bit clearer to understand.
    
     2. Move the creation of the threads into the setup function
    
          Move the `add_thread_from_worker` in `second` into `setup`
          function to setup all threads at the beginning of the demo.
    
     3. Change the MAX_THREADS from 3 to 4
    
          Since we move the creation of the threads into the `setup`
          function, we will have `setup`, `first`, `second` and `last` at
          the same time. So the MAX_THREADS has been changed to 4.
    
     4. Change one of the `yield()` functions to `yield_to()` in `second`
    
          Change the function from `yield()` to `yield_to()` to show that we
          can not only yield to higher but also lower tid.
    
     5. Modify the expected.log
    
          Revise the expected.log to fit the current behaviour of this demo.

--- demos/exyielding/exyielding.c
+++ demos/exyielding/exyielding.c
@@ -1,15 +1,14 @@
 #include "common/assert.h"
 #include "user/thread.h"
 
-void thread1() {
+void first() {
   yield_to(2);
 }
 
-void thread2() {
+void second() {
   yield_to(1);
-  add_named_thread(thread1, "last");
-  yield(); // switch to "last"
-  yield(); // switch back to "last"
+  yield_to(3); // switch to "last"
+  yield();     // switch back to "last"
   // Run ourselves again, shouldn't actually do a switch
   assert(!yield());
   assert(!yield_to(1));
@@ -18,6 +17,7 @@ void thread2() {
 void setup(void) {
   set_kernel_config(KCFG_LOG_SCHEDULER, 0);
 
-  add_named_thread(thread1, "first");
-  add_named_thread(thread2, "second");
+  add_named_thread(first, "first");
+  add_named_thread(second, "second");
+  add_named_thread(first, "last");
 }
