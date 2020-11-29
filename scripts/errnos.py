cae006f3e2514b539857181c6e4baf25ba30fb4a
David Spickett <spickettdavid@googlemail.com>
2020-11-29 11:47:05 +0000

    Correct typo in errno description

--- scripts/errnos.py
+++ scripts/errnos.py
@@ -1,6 +1,6 @@
 errnos = (
     # Name, number and string description
-    ("E_PERM",         1, "permisson denied"),
+    ("E_PERM",         1, "permission denied"),
     ("E_INVALID_ID",   2, "invalid thread ID"),
     ("E_NOT_FOUND",    3, "file not found"),
     ("E_NO_PAGE",      4, "no free code page"),
