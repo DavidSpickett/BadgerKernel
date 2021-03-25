5988b31511786ec2b27b294b226dee8e4d58ec97
David Spickett <spickettdavid@googlemail.com>
2021-03-25 16:39:20 +0000

    Avoid type warnings for syscall functions with -Wextra
    By assigning to a generic void* and only
    casting into the handler type when we call it.

--- src/kernel/syscall.c
+++ src/kernel/syscall.c
@@ -110,7 +110,6 @@ static void k_invalid_syscall(size_t arg1, size_t arg2, size_t arg3,
   k_exit(1);
 }
 
-typedef size_t (*SyscallFn)();
 void k_handle_syscall(void) {
   RegisterContext* ctx = (RegisterContext*)current_thread->stack_ptr;
 
@@ -122,7 +121,7 @@ void k_handle_syscall(void) {
     ctx->arg0 = INVALID_THREAD;
   }
 
-  SyscallFn syscall_fn = NULL;
+  void* syscall_fn = NULL;
   bool has_result = true;
   bool update_user_thread_info = false;
   size_t result = 0;
@@ -133,7 +132,7 @@ void k_handle_syscall(void) {
   cog.outl("switch (ctx->syscall_num) {")
   for syscall, has_result, update_user_thread_info in syscalls:
     cog.outl("  case syscall_{}:".format(syscall))
-    cog.outl("    syscall_fn = (SyscallFn)k_{};".format(syscall))
+    cog.outl("    syscall_fn = k_{};".format(syscall))
     cog.outl("    has_result = {};".format("true" if has_result else "false"))
     cog.outl("    update_user_thread_info = {};".format(
       "true" if update_user_thread_info else "false"))
@@ -141,117 +140,117 @@ void k_handle_syscall(void) {
   ]]] */
   switch (ctx->syscall_num) {
     case syscall_add_thread:
-      syscall_fn = (SyscallFn)k_add_thread;
+      syscall_fn = k_add_thread;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_get_thread_property:
-      syscall_fn = (SyscallFn)k_get_thread_property;
+      syscall_fn = k_get_thread_property;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_set_thread_property:
-      syscall_fn = (SyscallFn)k_set_thread_property;
+      syscall_fn = k_set_thread_property;
       has_result = true;
       update_user_thread_info = true;
       break;
     case syscall_set_kernel_config:
-      syscall_fn = (SyscallFn)k_set_kernel_config;
+      syscall_fn = k_set_kernel_config;
       has_result = false;
       update_user_thread_info = true;
       break;
     case syscall_yield:
-      syscall_fn = (SyscallFn)k_yield;
+      syscall_fn = k_yield;
       has_result = false;
       update_user_thread_info = false;
       break;
     case syscall_get_msg:
-      syscall_fn = (SyscallFn)k_get_msg;
+      syscall_fn = k_get_msg;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_send_msg:
-      syscall_fn = (SyscallFn)k_send_msg;
+      syscall_fn = k_send_msg;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_thread_wait:
-      syscall_fn = (SyscallFn)k_thread_wait;
+      syscall_fn = k_thread_wait;
       has_result = false;
       update_user_thread_info = false;
       break;
     case syscall_thread_wake:
-      syscall_fn = (SyscallFn)k_thread_wake;
+      syscall_fn = k_thread_wake;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_thread_cancel:
-      syscall_fn = (SyscallFn)k_thread_cancel;
+      syscall_fn = k_thread_cancel;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_mutex:
-      syscall_fn = (SyscallFn)k_mutex;
+      syscall_fn = k_mutex;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_condition_variable:
-      syscall_fn = (SyscallFn)k_condition_variable;
+      syscall_fn = k_condition_variable;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_open:
-      syscall_fn = (SyscallFn)k_open;
+      syscall_fn = k_open;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_read:
-      syscall_fn = (SyscallFn)k_read;
+      syscall_fn = k_read;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_write:
-      syscall_fn = (SyscallFn)k_write;
+      syscall_fn = k_write;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_lseek:
-      syscall_fn = (SyscallFn)k_lseek;
+      syscall_fn = k_lseek;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_remove:
-      syscall_fn = (SyscallFn)k_remove;
+      syscall_fn = k_remove;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_close:
-      syscall_fn = (SyscallFn)k_close;
+      syscall_fn = k_close;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_exit:
-      syscall_fn = (SyscallFn)k_exit;
+      syscall_fn = k_exit;
       has_result = false;
       update_user_thread_info = false;
       break;
     case syscall_malloc:
-      syscall_fn = (SyscallFn)k_malloc;
+      syscall_fn = k_malloc;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_realloc:
-      syscall_fn = (SyscallFn)k_realloc;
+      syscall_fn = k_realloc;
       has_result = true;
       update_user_thread_info = false;
       break;
     case syscall_free:
-      syscall_fn = (SyscallFn)k_free;
+      syscall_fn = k_free;
       has_result = false;
       update_user_thread_info = false;
       break;
     case syscall_list_dir:
-      syscall_fn = (SyscallFn)k_list_dir;
+      syscall_fn = k_list_dir;
       has_result = true;
       update_user_thread_info = false;
       break;
@@ -271,7 +270,9 @@ void k_handle_syscall(void) {
     user_thread_info.err_no = 0;
   }
 
-  result = syscall_fn(ctx->arg0, ctx->arg1, ctx->arg2, ctx->arg3);
+  typedef size_t (*SyscallFn)();
+  result =
+      ((SyscallFn)(syscall_fn))(ctx->arg0, ctx->arg1, ctx->arg2, ctx->arg3);
   // Make sure we zero out the result if the syscall returned void
   // So we don't leak some in kernel address
   if (!has_result) {
