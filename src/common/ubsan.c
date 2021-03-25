103a8b032d4adf34116ea854459f6f031eeb4af6
David Spickett <spickettdavid@googlemail.com>
2021-03-25 17:05:55 +0000

    Mark UBSAN handler args unused to silence -Wunused-parameter warnings

--- src/common/ubsan.c
+++ src/common/ubsan.c
@@ -24,16 +24,17 @@ typedef struct {
    timeout)
 */
 
-ubhandler(divrem_overflow, void* a, void* b);
-ubhandler(add_overflow, void* a, void* b);
-ubhandler(sub_overflow, void* a, void* b);
-ubhandler(load_invalid_value, void* a);
-ubhandler(nonnull_arg, void* a);
+#define UNUSED __attribute__((unused))
+ubhandler(divrem_overflow, UNUSED void* a, UNUSED void* b);
+ubhandler(add_overflow, UNUSED void* a, UNUSED void* b);
+ubhandler(sub_overflow, UNUSED void* a, UNUSED void* b);
+ubhandler(load_invalid_value, UNUSED void* a);
+ubhandler(nonnull_arg, UNUSED void* a);
 ubhandler(builtin_unreachable);
-ubhandler(type_mismatch, void* a);
-ubhandler(out_of_bounds, void* a);
-ubhandler(vla_bound_not_positive, void* a);
-ubhandler(shift_out_of_bounds, void* a, void* b);
-ubhandler(mul_overflow, void* a, void* b);
-ubhandler(type_mismatch_v1, void* a, void* b);
-ubhandler(pointer_overflow, void* a, void* b, void* c);
+ubhandler(type_mismatch, UNUSED void* a);
+ubhandler(out_of_bounds, UNUSED void* a);
+ubhandler(vla_bound_not_positive, UNUSED void* a);
+ubhandler(shift_out_of_bounds, UNUSED void* a, UNUSED void* b);
+ubhandler(mul_overflow, UNUSED void* a, UNUSED void* b);
+ubhandler(type_mismatch_v1, UNUSED void* a, UNUSED void* b);
+ubhandler(pointer_overflow, UNUSED void* a, UNUSED void* b, UNUSED void* c);
