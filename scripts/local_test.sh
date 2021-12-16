9f7a0a23d6488455d964da9fe7f38942d94c7a1d
David Spickett <spickettdavid@googlemail.com>
2021-12-16 20:50:13 +0000

    Disable globbing warning from shellcheck
    In this case we do want the output of find to become separate
    arguments to rm.

--- scripts/local_test.sh
+++ scripts/local_test.sh
@@ -9,6 +9,7 @@ for platform in "arm" "thumb" "aarch64" "raspi4"; do
         if [[ "${opt_level}_${lto}" != "0_ON" ]]; then
           rm -f CMakeCache.txt
           # Since test.sh is a config generated file not a build generated file, clean will not remove it.
+          # shellcheck disable=SC2046
           rm -f $(find demos/ -name test.sh)
           cmake . -G Ninja -DBUILD_PLATFORM=${platform} -DOPT_LEVEL=${opt_level} -DSANITIZERS=${sanitizers} -DLTO=${lto}
           ninja demos
