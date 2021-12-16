d99c9dde4e54bf1250e7a98743a77c1611b1e41e
David Spickett <spickettdavid@googlemail.com>
2021-12-16 20:38:29 +0000

    Remove test.sh files between runs in local test script
    Since these are configure time files they are not deleted by
    ninja clean. They will be updated if the config changes,
    but if you go from having the test enabled to disabled,
    the file is not deleted.
    
    This manually does that.

--- scripts/local_test.sh
+++ scripts/local_test.sh
@@ -8,6 +8,8 @@ for platform in "arm" "thumb" "aarch64" "raspi4"; do
         # LTO isn't enabled at O0
         if [[ "${opt_level}_${lto}" != "0_ON" ]]; then
           rm -f CMakeCache.txt
+          # Since test.sh is a config generated file not a build generated file, clean will not remove it.
+          rm -f $(find demos/ -name test.sh)
           cmake . -G Ninja -DBUILD_PLATFORM=${platform} -DOPT_LEVEL=${opt_level} -DSANITIZERS=${sanitizers} -DLTO=${lto}
           ninja demos
           lit demos/ -a
